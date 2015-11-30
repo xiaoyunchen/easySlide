b7ab5fdee2ac09bac4320ec5b1fec8a01e940c92
陈云 <1004183222@qq.com>
2015-11-30 10:44:45 +0800

    完善配置

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,26 @@ var path=require('path');
 var webpack = require('webpack');
 module.exports = {
     entry: {
-    		index:"./src/js/page/index.js"
+    		index:"./src/js/page/index.js",
     	},
     output: {
         path: path.join(__dirname,'dist'),
         filename: "bundle.js"
     },
     module: {
-        loaders: [
+        loaders: [	//css加载器
             { test: /\.css$/, loader: "style!css" }
         ]
     },
     plugins:[
-    	new webpack.ProvidePlugin({
+    	new webpack.ProvidePlugin({	//加载jq
             $: 'jquery'
-        })
+        }),
+        new webpack.optimize.UglifyJsPlugin({	//压缩代码
+		    compress: {
+		        warnings: false
+		    },
+		    except: ['$super', '$', 'exports', 'require']	//排除关键字
+		})
     ]
 };
